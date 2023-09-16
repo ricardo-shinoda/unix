e617c6cca12e67bb21239cf257323da085c019d2
ricardo-shinoda <ricardoshinoda@gmail.com>
2023-09-15 21:18:47 -0300

    undestanding how variables work on bash

---
+++ variable.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "What is your name?"
+
+read name
+
+echo "Hey $name!"
+sleep 2
+echo "You're looking good today $name"
+sleep 3
+echo "I hope you, $name, have a wonderfull day"
+
