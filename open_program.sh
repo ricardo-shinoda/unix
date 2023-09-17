e1a924427383c7b9b15e51a3f98da24106bb666e
ricardo-shinoda <ricardoshinoda@gmail.com>
2023-09-16 21:56:15 -0300

    edited: change name and fix: add nohup to the firefox openning

---
+++ open_program.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#check if the Slack desktop app is intalled
+if [ -x "$(command -v slack)" ]; then
+	nohup slack >/dev/null 2>&1 &
+else
+	echo "Slack desktop app is not installed on this system"
+
+fi
+
+#Check if Firefox is installed and open it
+if [ -x "$(command -v firefox)" ]; then
+	nohup firefox &
+else
+	echo "Firefox is not installed on this system."
+
+fi
