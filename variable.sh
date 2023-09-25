7122baf9b604c6c47ec7ffc22bbd4c883c88331f
Ricardo Shinoda <ricardoshinoda@gmail.com>
2023-09-24 22:13:14 -0300

    loop conditions

--- variable.sh
+++ variable.sh
@@ -9,4 +9,9 @@ sleep 2
 echo "You're looking good today $name"
 sleep 3
 echo "I hope you, $name, have a wonderfull day"
+MY_VARIABLE="Hello World"
 
+echo $MY_VARIABLE
+CURRENT_DATE=$(date)
+sleep 1
+echo "Right now is: " $CURRENT_DATE
