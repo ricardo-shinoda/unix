94636f9389930ef79d2710914c73035730b8791e
ricardo-shinoda <ricardoshinoda@gmail.com>
2023-10-14 23:31:52 -0300

    fix: script initial-linux

---
+++ initial-linux.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+#check if the Slack desktop app is intalled
+if [ -x "$(command -v slack)" ]; then
+	nohup slack >/dev/null 2>&1 &
+else
+	echo "Slack desktop app is not installed on this system"
+
+fi
+
+#Check if Google Chrome is installed and open it
+if [ -x "$(command -v google-chrome-stable)" ]; then
+	if pgrep -x "chrome" > /dev/null; then
+		echo "Google is already running"
+	else
+	nohup google-chrome-stable &
+	fi
+else
+	echo "Chrome is not installed on this system."
+
+fi
+
+# Check and open Vivaldi
+if [ -x "$(command -v vivaldi)" ]; then
+	if pgrep -x "vivaldi" > /dev/null; then
+		echo "Vivaldi is already running"
+	else
+	nohup vivaldi &
+	fi
+else
+	echo "Vivaldi is not installed on this system"
+fi
